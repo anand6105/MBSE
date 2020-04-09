d47dd6db41b6d838d9709cffb944818b946b11d9
anand6105 <anandprakaas@gmail.com>
2020-04-09 23:14:29 +0200

    Added macros to the header file

--- inc/mbse.h
+++ inc/mbse.h
@@ -8,6 +8,11 @@
 #ifndef MBSE_H_
 #define MBSE_H_
 
-#define WC_PATH   10
+#define MBSE_NUMBER_OF_THREADS       2
+#define MBSE_THREAD_STACK_SIZE       (100 * 1024)      /* 100 kB is enough for now. */
+
+#define MICRO_SECONDS                1000
+#define MILLI_SECONDS                (MICRO_SECONDS * 1000)
+#define SECONDS                      (MILLI_SECONDS * 1000)
 
 #endif /* MBSE_H_ */
